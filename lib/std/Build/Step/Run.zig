b6514e8a576c3614f6ff1474f28c77a6b54ab0d4
Nico Elbers <nico.b.elbers@gmail.com>
2024-06-05 03:57:11 +0200

    Add skip reason to build summary
    Add a message to why a step was skipped to the build summary by adding
    an optional result_skip_reason field to Step, and setting it whenever
    the MakeSkipped error is returned.
    
    Solved https://github.com/ziglang/zig/issues/14965

--- lib/std/Build/Step/Run.zig
+++ lib/std/Build/Step/Run.zig
@@ -1317,7 +1317,10 @@ fn runCommand(
                 .link_libc = exe.is_linking_libc,
             })) {
                 .native, .rosetta => {
-                    if (allow_skip) return error.MakeSkipped;
+                    if (allow_skip) {
+                        run.step.result_skip_reason = "Invalid binary";
+                        return error.MakeSkipped;
+                    }
                     break :interpret;
                 },
                 .wine => |bin_name| {
@@ -1395,7 +1398,10 @@ fn runCommand(
                     }
                 },
                 .bad_dl => |foreign_dl| {
-                    if (allow_skip) return error.MakeSkipped;
+                    if (allow_skip) {
+                        run.step.result_skip_reason = "Invalid binary";
+                        return error.MakeSkipped;
+                    }
 
                     const host_dl = b.graph.host.result.dynamic_linker.get() orelse "(none)";
 
@@ -1407,7 +1413,10 @@ fn runCommand(
                     , .{ host_dl, foreign_dl });
                 },
                 .bad_os_or_cpu => {
-                    if (allow_skip) return error.MakeSkipped;
+                    if (allow_skip) {
+                        run.step.result_skip_reason = "Invalid os or cpu";
+                        return error.MakeSkipped;
+                    }
 
                     const host_name = try b.graph.host.result.zigTriple(b.allocator);
                     const foreign_name = try root_target.zigTriple(b.allocator);
@@ -1428,8 +1437,12 @@ fn runCommand(
             try Step.handleVerbose2(step.owner, cwd, run.env_map, interp_argv.items);
 
             break :term spawnChildAndCollect(run, interp_argv.items, &env_map, has_side_effects, options, fuzz_context) catch |e| {
-                if (!run.failing_to_execute_foreign_is_an_error) return error.MakeSkipped;
+                if (!run.failing_to_execute_foreign_is_an_error) {
+                    run.step.result_skip_reason = "Foreign binary failed";
+                    return error.MakeSkipped;
+                }
                 if (e == error.MakeFailed) return error.MakeFailed; // error already reported
+
                 return step.fail("unable to spawn interpreter {s}: {s}", .{
                     interp_argv.items[0], @errorName(e),
                 });
@@ -2294,8 +2307,10 @@ fn failForeign(
 ) error{ MakeFailed, MakeSkipped, OutOfMemory } {
     switch (run.stdio) {
         .check, .zig_test => {
-            if (run.skip_foreign_checks)
+            if (run.skip_foreign_checks) {
+                run.step.result_skip_reason = "Foreign binary failed";
                 return error.MakeSkipped;
+            }
 
             const b = run.step.owner;
             const host_name = try b.graph.host.result.zigTriple(b.allocator);
