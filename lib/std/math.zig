8031e4c70a754807b2cde2f26d32e756754be433
Rue04 <iokg04@gmail.com>
2025-12-07 00:27:45 +0100

    `std.math`: move `wrap` vector tests to extra function to skip on hexagon arch
    There currently seems to be an issue with `@mod` and vectors on hexagon:
    https://codeberg.org/ziglang/zig/pulls/30114#issuecomment-8737539

--- lib/std/math.zig
+++ lib/std/math.zig
@@ -485,56 +485,37 @@ pub fn wrap(x: anytype, r: anytype) @TypeOf(x) {
     }
 }
 test wrap {
-    const Vi32 = @Vector(4, i32);
-    const Vf32 = @Vector(4, f32);
-
     // Within range
     try testing.expect(wrap(@as(i32, -75), @as(i32, 180)) == -75);
     try testing.expect(wrap(@as(i32, -75), @as(i32, -180)) == -75);
-    try testing.expect(@reduce(.And, wrap(@as(Vi32, @splat(-75)), @as(Vi32, @splat(180))) == @as(Vi32, @splat(-75))));
-    try testing.expect(@reduce(.And, wrap(@as(Vi32, @splat(-75)), @as(Vi32, @splat(-180))) == @as(Vi32, @splat(-75))));
     // Below
     try testing.expect(wrap(@as(i32, -225), @as(i32, 180)) == 135);
     try testing.expect(wrap(@as(i32, -225), @as(i32, -180)) == 135);
-    try testing.expect(@reduce(.And, wrap(@as(Vi32, @splat(-225)), @as(Vi32, @splat(180))) == @as(Vi32, @splat(135))));
-    try testing.expect(@reduce(.And, wrap(@as(Vi32, @splat(-225)), @as(Vi32, @splat(-180))) == @as(Vi32, @splat(135))));
     // Above
     try testing.expect(wrap(@as(i32, 361), @as(i32, 180)) == 1);
     try testing.expect(wrap(@as(i32, 361), @as(i32, -180)) == 1);
-    try testing.expect(@reduce(.And, wrap(@as(Vi32, @splat(361)), @as(Vi32, @splat(180))) == @as(Vi32, @splat(1))));
-    try testing.expect(@reduce(.And, wrap(@as(Vi32, @splat(361)), @as(Vi32, @splat(-180))) == @as(Vi32, @splat(1))));
 
     // One period, right limit, positive r
     try testing.expect(wrap(@as(i32, 180), @as(i32, 180)) == -180);
-    try testing.expect(@reduce(.And, wrap(@as(Vi32, @splat(180)), @as(Vi32, @splat(180))) == @as(Vi32, @splat(-180))));
     // One period, left limit, positive r
     try testing.expect(wrap(@as(i32, -180), @as(i32, 180)) == -180);
-    try testing.expect(@reduce(.And, wrap(@as(Vi32, @splat(-180)), @as(Vi32, @splat(180))) == @as(Vi32, @splat(-180))));
     // One period, right limit, negative r
     try testing.expect(wrap(@as(i32, 180), @as(i32, -180)) == 180);
-    try testing.expect(@reduce(.And, wrap(@as(Vi32, @splat(180)), @as(Vi32, @splat(-180))) == @as(Vi32, @splat(180))));
     // One period, left limit, negative r
     try testing.expect(wrap(@as(i32, -180), @as(i32, -180)) == 180);
-    try testing.expect(@reduce(.And, wrap(@as(Vi32, @splat(-180)), @as(Vi32, @splat(-180))) == @as(Vi32, @splat(180))));
 
     // Two periods, right limit, positive r
     try testing.expect(wrap(@as(i32, 540), @as(i32, 180)) == -180);
-    try testing.expect(@reduce(.And, wrap(@as(Vi32, @splat(540)), @as(Vi32, @splat(180))) == @as(Vi32, @splat(-180))));
     // Two periods, left limit, positive r
     try testing.expect(wrap(@as(i32, -540), @as(i32, 180)) == -180);
-    try testing.expect(@reduce(.And, wrap(@as(Vi32, @splat(-540)), @as(Vi32, @splat(180))) == @as(Vi32, @splat(-180))));
     // Two periods, right limit, negative r
     try testing.expect(wrap(@as(i32, 540), @as(i32, -180)) == 180);
-    try testing.expect(@reduce(.And, wrap(@as(Vi32, @splat(540)), @as(Vi32, @splat(-180))) == @as(Vi32, @splat(180))));
     // Two periods, left limit, negative r
     try testing.expect(wrap(@as(i32, -540), @as(i32, -180)) == 180);
-    try testing.expect(@reduce(.And, wrap(@as(Vi32, @splat(-540)), @as(Vi32, @splat(-180))) == @as(Vi32, @splat(180))));
 
     // Floating point
     try testing.expect(wrap(@as(f32, 1.125), @as(f32, 1.0)) == -0.875);
     try testing.expect(wrap(@as(f32, -127.5), @as(f32, 180)) == -127.5);
-    try testing.expect(@reduce(.And, wrap(@as(Vf32, @splat(1.125)), @as(Vf32, @splat(1.0))) == @as(Vf32, @splat(-0.875))));
-    try testing.expect(@reduce(.And, wrap(@as(Vf32, @splat(-127.5)), @as(Vf32, @splat(180))) == @as(Vf32, @splat(-127.5))));
 
     // Mix of comptime and non-comptime
     var i: i32 = 1;
@@ -548,6 +529,46 @@ test wrap {
     try testing.expect(wrap(@as(i32, -225), limit) == 135);
     // Above
     try testing.expect(wrap(@as(i32, 361), limit) == 1);
+
+    try testWrapVector();
+}
+fn testWrapVector() !void {
+    const Vi32 = @Vector(4, i32);
+    const Vf32 = @Vector(4, f32);
+
+    if (builtin.target.cpu.arch == .hexagon) return; // https://codeberg.org/ziglang/zig/pulls/30114#issuecomment-8737539
+
+    // Within range
+    try testing.expect(@reduce(.And, wrap(@as(Vi32, @splat(-75)), @as(Vi32, @splat(180))) == @as(Vi32, @splat(-75))));
+    try testing.expect(@reduce(.And, wrap(@as(Vi32, @splat(-75)), @as(Vi32, @splat(-180))) == @as(Vi32, @splat(-75))));
+    // Below
+    try testing.expect(@reduce(.And, wrap(@as(Vi32, @splat(-225)), @as(Vi32, @splat(180))) == @as(Vi32, @splat(135))));
+    try testing.expect(@reduce(.And, wrap(@as(Vi32, @splat(-225)), @as(Vi32, @splat(-180))) == @as(Vi32, @splat(135))));
+    // Above
+    try testing.expect(@reduce(.And, wrap(@as(Vi32, @splat(361)), @as(Vi32, @splat(180))) == @as(Vi32, @splat(1))));
+    try testing.expect(@reduce(.And, wrap(@as(Vi32, @splat(361)), @as(Vi32, @splat(-180))) == @as(Vi32, @splat(1))));
+
+    // One period, right limit, positive r
+    try testing.expect(@reduce(.And, wrap(@as(Vi32, @splat(180)), @as(Vi32, @splat(180))) == @as(Vi32, @splat(-180))));
+    // One period, left limit, positive r
+    try testing.expect(@reduce(.And, wrap(@as(Vi32, @splat(-180)), @as(Vi32, @splat(180))) == @as(Vi32, @splat(-180))));
+    // One period, right limit, negative r
+    try testing.expect(@reduce(.And, wrap(@as(Vi32, @splat(180)), @as(Vi32, @splat(-180))) == @as(Vi32, @splat(180))));
+    // One period, left limit, negative r
+    try testing.expect(@reduce(.And, wrap(@as(Vi32, @splat(-180)), @as(Vi32, @splat(-180))) == @as(Vi32, @splat(180))));
+
+    // Two periods, right limit, positive r
+    try testing.expect(@reduce(.And, wrap(@as(Vi32, @splat(540)), @as(Vi32, @splat(180))) == @as(Vi32, @splat(-180))));
+    // Two periods, left limit, positive r
+    try testing.expect(@reduce(.And, wrap(@as(Vi32, @splat(-540)), @as(Vi32, @splat(180))) == @as(Vi32, @splat(-180))));
+    // Two periods, right limit, negative r
+    try testing.expect(@reduce(.And, wrap(@as(Vi32, @splat(540)), @as(Vi32, @splat(-180))) == @as(Vi32, @splat(180))));
+    // Two periods, left limit, negative r
+    try testing.expect(@reduce(.And, wrap(@as(Vi32, @splat(-540)), @as(Vi32, @splat(-180))) == @as(Vi32, @splat(180))));
+
+    // Floating point
+    try testing.expect(@reduce(.And, wrap(@as(Vf32, @splat(1.125)), @as(Vf32, @splat(1.0))) == @as(Vf32, @splat(-0.875))));
+    try testing.expect(@reduce(.And, wrap(@as(Vf32, @splat(-127.5)), @as(Vf32, @splat(180))) == @as(Vf32, @splat(-127.5))));
 }
 
 /// Odd ramp function
