c17efb5cde1cdd7b50cd6b8866f229339c658e1b
Rue04 <iokg04@gmail.com>
2025-12-27 16:14:58 +0100

    tests: add more tests for eu and option to/from `comptime_int`
    https://codeberg.org/ziglang/zig/pulls/30603#issuecomment-9286058

--- test/cases/compile_errors/coercion_from_eu_or_optional_containing_or_to_comptime_int.zig
+++ test/cases/compile_errors/coercion_from_eu_or_optional_containing_or_to_comptime_int.zig
@@ -10,7 +10,21 @@ comptime {
     _ = i;
 }
 
+comptime {
+    const op: anyerror!comptime_int = 10;
+    const i: u8 = op;
+    _ = i;
+}
+
+comptime {
+    const op: ?comptime_int = 10;
+    const i: u8 = op;
+    _ = i;
+}
+
 // error
 //
 // :3:29: error: expected type 'comptime_int', found 'anyerror!u8'
 // :9:29: error: expected type 'comptime_int', found '?u8'
+// :15:19: error: expected type 'u8', found 'anyerror!comptime_int'
+// :21:19: error: expected type 'u8', found '?comptime_int'
