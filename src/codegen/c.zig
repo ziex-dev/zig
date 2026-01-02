336d8980dc5bfb32bdfe57c50717c1ad2ed02bc6
Laurin-Luis Lehning <65224843+e820@users.noreply.github.com>
2026-01-02 10:52:56 +0100

    Add comment

--- src/codegen/c.zig
+++ src/codegen/c.zig
@@ -5529,6 +5529,8 @@ fn airAsm(f: *Function, inst: Air.Inst.Index) !CValue {
                     .alignas = CType.AlignAs.fromAbiAlignment(input_ty.abiAlignment(zcu)),
                 });
                 try f.allocs.put(gpa, input_local.new_local, false);
+                // Do not render the declaration as `const` qualified if we're generating an
+                // explicit `register` local, as GCC will ignore the constraint completely.
                 try f.object.dg.renderTypeAndName(w, input_ty, input_local, if (is_reg) .{} else Const, .none, .complete);
                 if (is_reg) {
                     try w.writeAll(" __asm(\"");
