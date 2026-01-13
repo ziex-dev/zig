9bbdbb3b4c417d0c9b9d3b8c7470533ca0f286f9
Chris Waldon <christopher.waldon.dev@gmail.com>
2026-01-13 07:58:07 -0500

    doc: clarify inline for capture comptime availability
    This commit tries to clarify that all capture values in an inline for loop
    are not necessarily comptime-known. The previous wording seemed to imply
    that they were, whereas the actual guarantee is that they will be comptime-
    known only if their source is comptime known.
    
    The wording of this commit comes from mlugg, as they are a more knowledgeable
    source on the semantics than I am.
    
    Closes: https://codeberg.org/ziglang/zig/issues/30116
    
    Co-authored-by: Matthew Lugg <mlugg@mlugg.co.uk>
    Signed-off-by: Chris Waldon <christopher.waldon.dev@gmail.com>

--- doc/langref/test_inline_for.zig
+++ doc/langref/test_inline_for.zig
@@ -1,22 +1,28 @@
 const expect = @import("std").testing.expect;
+const Int = @import("std").meta.Int;
 
 test "inline for loop" {
-    const nums = [_]i32{ 2, 4, 6 };
-    var sum: usize = 0;
-    inline for (nums) |i| {
-        const T = switch (i) {
-            2 => f32,
-            4 => i8,
-            6 => bool,
-            else => unreachable,
-        };
-        sum += typeNameLength(T);
+    const array = getRuntimeArray();
+    const slice = getRuntimeSlice();
+    const len = slice.len;
+    // All of the above values are runtime-known, but the *length* of `array` is
+    // known at compile time, so we can use `inline for`. This will effectively
+    // duplicate the loop body 3 times.
+    inline for (0..len, array, slice) |i, array_elem, slice_elem| {
+        // Even though `len` was runtime-known, because the start index 0 was
+        // comptime-known, `i` is comptime-known. That means we can do things
+        // with it which are only legal at comptime, such as create types.
+        _ = Int(.unsigned, i);
+        // `slice_elem` and `arr_elem` are runtime-known.
+        try expect(array_elem == i + 5);
+        try expect(slice_elem == i * 10);
     }
-    try expect(sum == 9);
 }
-
-fn typeNameLength(comptime T: type) usize {
-    return @typeName(T).len;
+fn getRuntimeArray() [3]u8 {
+    return .{ 5, 6, 7 };
+}
+fn getRuntimeSlice() []const u8 {
+    return &.{ 0, 10, 20 };
 }
 
 // test
