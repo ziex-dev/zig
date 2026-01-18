722ab41245705a743acc37eb395542a9e4a2219a
just_some_entity <entity@jsentity.dev>
2026-01-18 09:49:55 +0100

    Let unreachable blocks cascade into else statement

--- lib/std/os/uefi/tables/boot_services.zig
+++ lib/std/os/uefi/tables/boot_services.zig
@@ -702,19 +702,19 @@ pub const BootServices = extern struct {
             &len,
             null,
         )) {
-	        .buffer_too_small => return @divExact(len, @sizeOf(uefi.Handle)),
-	        .not_found => return 0,
-	        .success => unreachable, // if len == 0, should return not_found, otherwise buffer_too_small
-	        .invalid_parameter => {
-	            // reasons:
-	            // - SearchType is not a member of EFI_LOCATE_SEARCH_TYPE. (Only possible if search is undefined)
-	            // - SearchType is ByRegisterNotify and SearchKey is NULL. (Accounted for in function call)
-	            // - SearchType is ByProtocol and ProtocoL is NULL. (Accounted for in function call)
-	            // - One or more matches are found and BufferSize is NULL. (len is not NULL, not possible)
-	            // - BufferSize is large enough for the result and Buffer is NULL. (BufferSize is always 0)
-	            unreachable;
-	        },
-	        else => |status| return uefi.unexpectedStatus(status),
+            .buffer_too_small => return @divExact(len, @sizeOf(uefi.Handle)),
+            .not_found => return 0,
+            // .success => unreachable, // if len == 0, should return not_found, otherwise buffer_too_small
+            //.invalid_parameter => {
+            // reasons:
+            // - SearchType is not a member of EFI_LOCATE_SEARCH_TYPE. (Only possible if search is undefined)
+            // - SearchType is ByRegisterNotify and SearchKey is NULL. (Accounted for in function call)
+            // - SearchType is ByProtocol and ProtocoL is NULL. (Accounted for in function call)
+            // - One or more matches are found and BufferSize is NULL. (len is not NULL, not possible)
+            // - BufferSize is large enough for the result and Buffer is NULL. (BufferSize is always 0)
+            // unreachable;
+            // },
+            else => |status| return uefi.unexpectedStatus(status),
         }
     }
 
