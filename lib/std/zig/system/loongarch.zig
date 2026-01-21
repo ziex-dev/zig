59c531e2d68b00a5bebcb4c8145d759d3d17fe83
bartimaeusnek <33183715+bartimaeusnek@users.noreply.github.com>
2026-01-21 20:46:14 +0100

    implement bit tests for loongarch

--- lib/std/zig/system/loongarch.zig
+++ lib/std/zig/system/loongarch.zig
@@ -1,6 +1,14 @@
 const builtin = @import("builtin");
 const std = @import("std");
 
+inline fn testBit(cfg: u32, bitToTest: i32) bool {
+    return cfg & (1 << bitToTest) != 0;
+}
+
+inline fn addFeature(cpu: *std.Target.Cpu, feature: std.Target.loongarch.Feature) void {
+    cpu.features.addFeature(@intFromEnum(feature));
+}
+
 pub fn detectNativeCpuAndFeatures(
     arch: std.Target.Cpu.Arch,
     os: std.Target.Os,
@@ -24,6 +32,30 @@ pub fn detectNativeCpuAndFeatures(
 
     cpu.features.addFeatureSet(cpu.model.features);
     cpu.features.populateDependencies(cpu.arch.allFeaturesList());
+
+    const cfg1 = cpucfg(1);
+    const cfg2 = cpucfg(2);
+    const cfg3 = cpucfg(3);
+
+    if (testBit(cfg1, 20)) addFeature(&cpu, std.Target.loongarch.Feature.ual);
+
+    const hasFpu = testBit(cfg2, 0);
+    if (hasFpu) {
+        if (testBit(cfg2,1)) addFeature(&cpu, std.Target.loongarch.Feature.f);
+        if (testBit(cfg2,2)) addFeature(&cpu, std.Target.loongarch.Feature.d);
+    }
+
+    if (testBit(cfg2, 6)) addFeature(&cpu, std.Target.loongarch.Feature.lsx);
+    if (testBit(cfg2, 7)) addFeature(&cpu, std.Target.loongarch.Feature.lasx);
+    if (testBit(cfg2, 10)) addFeature(&cpu, std.Target.loongarch.Feature.lvz);
+
+    if (testBit(cfg2, 25)) addFeature(&cpu, std.Target.loongarch.Feature.frecipe);
+    if (testBit(cfg2, 26)) addFeature(&cpu, std.Target.loongarch.Feature.div32);
+    if (testBit(cfg2, 27)) addFeature(&cpu, std.Target.loongarch.Feature.lam_bh);
+    if (testBit(cfg2, 28)) addFeature(&cpu, std.Target.loongarch.Feature.lamcas);
+    if (testBit(cfg2, 30)) addFeature(&cpu, std.Target.loongarch.Feature.scq);
+
+    if (testBit(cfg3, 23)) addFeature(&cpu, std.Target.loongarch.Feature.ld_seq_sa);
 
     return cpu;
 }
