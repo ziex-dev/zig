b5d8894eded091dd5b2610d98660d925096e8018
Andrew Kelley <andrew@ziglang.org>
2026-01-21 21:42:23 -0800

    std.Io.Threaded: don't pass null to NtDelayExecution
    Windows returns ACCESS_VIOLATION if you do that.

--- lib/std/Io/Threaded.zig
+++ lib/std/Io/Threaded.zig
@@ -8442,6 +8442,7 @@ fn fileReadStreamingWindows(file: File, data: []const []u8) File.Reader.Error!us
 
     var io_status_block: windows.IO_STATUS_BLOCK = undefined;
     var done: bool = false;
+    const infinite: windows.LARGE_INTEGER = windows.INFINITE;
 
     read: {
         const syscall: Syscall = try .start();
@@ -8470,7 +8471,7 @@ fn fileReadStreamingWindows(file: File, data: []const []u8) File.Reader.Error!us
         }
         try syscall.toApc(.{ .handle = file.handle, .iosb = &io_status_block });
         while (true) {
-            switch (windows.ntdll.NtDelayExecution(1, null)) {
+            switch (windows.ntdll.NtDelayExecution(1, &infinite)) {
                 .USER_APC => {
                     if (!done) {
                         // Other APC work was queued before calling into this function.
@@ -8479,7 +8480,7 @@ fn fileReadStreamingWindows(file: File, data: []const []u8) File.Reader.Error!us
                     }
                     break syscall.finishApc();
                 },
-                .SUCCESS, .CANCELLED => {
+                .SUCCESS, .CANCELLED, .TIMEOUT, .ALERTED => {
                     try syscall.checkCancelApc();
                     continue;
                 },
