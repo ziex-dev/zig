29e40cdd118a5301ec8c17f7b07dfcb5c0c8f9d6
Andrew Kelley <andrew@ziglang.org>
2026-01-21 19:16:35 -0800

    std.Io.Threaded: add some temporary, choice panics

--- lib/std/Io/Threaded.zig
+++ lib/std/Io/Threaded.zig
@@ -8396,7 +8396,8 @@ fn fileReadStreamingWindows(file: File, data: []const []u8) File.Reader.Error!us
                     continue;
                 },
                 .INVALID_PARAMETER => |err| return syscall.ntstatusBug(err), // streaming read of async mode file
-                else => |status| return syscall.unexpectedNtstatus(status),
+                else => |status| std.debug.panic("fileReadStreamingWindows NtReadFile returned {t}", .{status}),
+                //else => |status| return syscall.unexpectedNtstatus(status),
             }
         }
         try syscall.toApc();
@@ -8407,7 +8408,8 @@ fn fileReadStreamingWindows(file: File, data: []const []u8) File.Reader.Error!us
                     try syscall.checkCancel();
                     continue;
                 },
-                else => |status| return syscall.unexpectedNtstatus(status),
+                else => |status| std.debug.panic("fileReadStreamingWindows NtDelayExecution returned {t}", .{status}),
+                //else => |status| return syscall.unexpectedNtstatus(status),
             }
         }
     }
@@ -8415,7 +8417,8 @@ fn fileReadStreamingWindows(file: File, data: []const []u8) File.Reader.Error!us
     switch (io_status_block.u.Status) {
         .SUCCESS, .END_OF_FILE, .PIPE_BROKEN => {},
         .ACCESS_DENIED => return error.AccessDenied,
-        else => |status| return windows.unexpectedStatus(status),
+        else => |status| std.debug.panic("fileReadStreamingWindows IO_STATUS_BLOCK returned {t}", .{status}),
+        //else => |status| return windows.unexpectedStatus(status),
     }
     return io_status_block.Information;
 }
