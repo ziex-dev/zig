8f9a1d801e970252aee3774ed4c867c620a9e1cb
Andrew Kelley <andrew@ziglang.org>
2026-01-21 19:08:52 -0800

    std: back out the flags field of Io.File
    For now, let us refrain from putting the sync mode into the Io.File
    struct, and document that to do concurrent batch operations, any Windows
    file handles must be in asynchronous mode. The consequences for
    violating this requirement is neither illegal behavior, nor an error,
    but that concurrency is lost. In other words, deadlock might occur. This
    prevents the addition of flags field.
    
    partial revert of 2faf14200f58ee72ec3a13e894d765f59e6483a9

--- lib/std/Io/Threaded.zig
+++ lib/std/Io/Threaded.zig
@@ -2759,12 +2759,7 @@ fn dirCreateDirPathOpenWasi(
 
 fn dirStat(userdata: ?*anyopaque, dir: Dir) Dir.StatError!Dir.Stat {
     const t: *Threaded = @ptrCast(@alignCast(userdata));
-    const file: File = if (is_windows) .{
-        .handle = dir.handle,
-        .flags = .{ .nonblocking = false },
-    } else .{
-        .handle = dir.handle,
-    };
+    const file: File = .{ .handle = dir.handle };
     return fileStat(t, file);
 }
 
@@ -3686,10 +3681,7 @@ fn dirCreateFileWindows(
     errdefer windows.CloseHandle(handle);
 
     const exclusive = switch (flags.lock) {
-        .none => return .{
-            .handle = handle,
-            .flags = .{ .nonblocking = false },
-        },
+        .none => return .{ .handle = handle },
         .shared => false,
         .exclusive => true,
     };
@@ -3709,10 +3701,7 @@ fn dirCreateFileWindows(
     )) {
         .SUCCESS => {
             syscall.finish();
-            return .{
-                .handle = handle,
-                .flags = .{ .nonblocking = false },
-            };
+            return .{ .handle = handle };
         },
         .INSUFFICIENT_RESOURCES => return syscall.fail(error.SystemResources),
         .LOCK_NOT_GRANTED => return syscall.fail(error.WouldBlock),
@@ -4287,10 +4276,7 @@ pub fn dirOpenFileWtf16(
     errdefer w.CloseHandle(handle);
 
     const exclusive = switch (flags.lock) {
-        .none => return .{
-            .handle = handle,
-            .flags = .{ .nonblocking = false },
-        },
+        .none => return .{ .handle = handle },
         .shared => false,
         .exclusive => true,
     };
@@ -4313,10 +4299,7 @@ pub fn dirOpenFileWtf16(
         .ACCESS_VIOLATION => |err| return syscall.ntstatusBug(err), // bad io_status_block pointer
         else => |status| return syscall.unexpectedNtstatus(status),
     };
-    return .{
-        .handle = handle,
-        .flags = .{ .nonblocking = false },
-    };
+    return .{ .handle = handle };
 }
 
 fn dirOpenFileWasi(
@@ -8271,7 +8254,7 @@ fn fileReadStreamingWindows(file: File, data: []const []u8) File.Reader.Error!us
                     try syscall.checkCancel();
                     continue;
                 },
-                .INVALID_PARAMETER => |err| return syscall.ntstatusBug(err), // wrong value for flags.nonblocking
+                .INVALID_PARAMETER => |err| return syscall.ntstatusBug(err), // streaming read of async mode file
                 else => |status| return syscall.unexpectedNtstatus(status),
             }
         }
@@ -14363,9 +14346,9 @@ fn processSpawnWindows(userdata: ?*anyopaque, options: process.SpawnOptions) pro
     return .{
         .id = piProcInfo.hProcess,
         .thread_handle = piProcInfo.hThread,
-        .stdin = if (g_hChildStd_IN_Wr) |h| .{ .handle = h, .flags = .{ .nonblocking = true } } else null,
-        .stdout = if (g_hChildStd_OUT_Rd) |h| .{ .handle = h, .flags = .{ .nonblocking = true } } else null,
-        .stderr = if (g_hChildStd_ERR_Rd) |h| .{ .handle = h, .flags = .{ .nonblocking = true } } else null,
+        .stdin = if (g_hChildStd_IN_Wr) |h| .{ .handle = h } else null,
+        .stdout = if (g_hChildStd_OUT_Rd) |h| .{ .handle = h } else null,
+        .stderr = if (g_hChildStd_ERR_Rd) |h| .{ .handle = h } else null,
         .request_resource_usage_statistics = options.request_resource_usage_statistics,
     };
 }
@@ -15499,7 +15482,6 @@ fn progressParentFile(userdata: ?*anyopaque) std.Progress.ParentFileError!File {
             .pointer => @ptrFromInt(int),
             else => return error.UnsupportedOperation,
         },
-        .flags = if (is_windows) .{ .nonblocking = true } else .{},
     };
 }
 
