5337ac9e4f0a5adb86b6929f3254f4129e966907
Lukas Lalinsky <lukas@lalinsky.com>
2026-01-26 18:19:56 +0100

    Open WSA sockets with WSA_FLAG_OVERLAPPED
    Even though we are not using overlapped I/O, we still need to set the
    WSA_FLAG_OVERLAPPED flag on the socket, to allow multiple threads
    using it concurrently.
    
    See:
    
    https://discord.com/channels/605571803288698900/1039652415282094090/1465390322887561403
    
    https://stackoverflow.com/questions/65238091/why-would-a-call-to-wsasocket-vs-socket-cause-sends-to-block

--- lib/std/Io/Threaded.zig
+++ lib/std/Io/Threaded.zig
@@ -11025,7 +11025,7 @@ fn openSocketWsa(
 ) !ws2_32.SOCKET {
     const mode = posixSocketMode(options.mode);
     const protocol = posixProtocol(options.protocol);
-    const flags: u32 = ws2_32.WSA_FLAG_NO_HANDLE_INHERIT;
+    const flags: u32 = ws2_32.WSA_FLAG_NO_HANDLE_INHERIT | ws2_32.WSA_FLAG_OVERLAPPED;
     var syscall: Syscall = try .start();
     while (true) {
         const rc = ws2_32.WSASocketW(family, @bitCast(mode), @bitCast(protocol), null, 0, flags);
