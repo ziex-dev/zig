988413ea4f6d260cf87cd52552ad7a5d4429223c
Pivok <pivoc@protonmail.com>
2026-02-11 19:39:40 +0100

    zig libc: exp10, exp10f, pow10, pow10f (#31163)
    See #30978.
    
    Commands i run:
    ```
    $ stage3/bin/zig build -p stage4 -Denable-llvm -Dno-lib
    
    $ stage4/bin/zig build test-libc -Dlibc-test-path=../../libc-test -Dtest-filter=exp10 -fqemu -fwasmtime --summary line
    Build Summary: 725/737 steps succeeded (12 skipped)
    ```
    
    Reviewed-on: https://codeberg.org/ziglang/zig/pulls/31163
    Reviewed-by: Andrew Kelley <andrew@ziglang.org>
    Co-authored-by: Pivok <pivoc@protonmail.com>
    Co-committed-by: Pivok <pivoc@protonmail.com>

--- lib/c/math.zig
+++ lib/c/math.zig
@@ -45,8 +45,12 @@ comptime {
         symbol(&atanl, "atanl");
         symbol(&cbrt, "cbrt");
         symbol(&cbrtf, "cbrtf");
+        symbol(&exp10, "exp10");
+        symbol(&exp10f, "exp10f");
         symbol(&hypot, "hypot");
         symbol(&pow, "pow");
+        symbol(&pow10, "pow10");
+        symbol(&pow10f, "pow10f");
     }
 
     if (builtin.target.isMuslLibC()) {
@@ -127,6 +131,14 @@ fn cbrtf(x: f32) callconv(.c) f32 {
     return math.cbrt(x);
 }
 
+fn exp10(x: f64) callconv(.c) f64 {
+    return math.pow(f64, 10.0, x);
+}
+
+fn exp10f(x: f32) callconv(.c) f32 {
+    return math.pow(f32, 10.0, x);
+}
+
 fn hypot(x: f64, y: f64) callconv(.c) f64 {
     return math.hypot(x, y);
 }
@@ -141,4 +153,12 @@ fn hypotl(x: c_longdouble, y: c_longdouble) callconv(.c) c_longdouble {
 
 fn pow(x: f64, y: f64) callconv(.c) f64 {
     return math.pow(f64, x, y);
+}
+
+fn pow10(x: f64) callconv(.c) f64 {
+    return exp10(x);
+}
+
+fn pow10f(x: f32) callconv(.c) f32 {
+    return exp10f(x);
 }
