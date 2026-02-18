dd5d23da9b83de735ff17911161d45e514782368
maci <maci@noreply.codeberg.org>
2026-02-18 10:20:36 +0100

    target: remove linking_libc

--- src/target.zig
+++ src/target.zig
@@ -53,8 +53,7 @@ pub fn libCxxNeedsLibUnwind(target: *const std.Target) bool {
 }
 
 /// This function returns whether non-pic code is completely invalid on the given target.
-pub fn requiresPIC(target: *const std.Target, linking_libc: bool) bool {
-    _ = linking_libc;
+pub fn requiresPIC(target: *const std.Target) bool {
     return target.abi.isAndroid() or
         ((target.os.tag == .windows or target.os.tag == .uefi) and (target.cpu.arch == .aarch64 or target.cpu.arch == .x86_64)) or
         target.requiresLibC();
