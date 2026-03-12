302e4128d7341fa2a67f8364d26732f4074ab798
squidy239 <sachabarsayuracko@gmail.com>
2026-03-12 10:01:27 -0500

    rename MemoryPoolExtra to MemoryPool and remove depricated versions

--- lib/std/heap.zig
+++ lib/std/heap.zig
@@ -20,21 +20,9 @@ pub const DebugAllocatorConfig = @import("heap/debug_allocator.zig").Config;
 pub const DebugAllocator = @import("heap/debug_allocator.zig").DebugAllocator;
 pub const Check = enum { ok, leak };
 
-/// A memory pool that can allocate objects of a single type very quickly.
-/// Use this when you need to allocate a lot of objects of the same type,
-/// because it outperforms general purpose allocators.
-/// Functions that potentially allocate memory accept an `Allocator` parameter.
-pub fn MemoryPool(comptime Item: type) type {
-    return memory_pool.Extra(Item, .{ .alignment = null });
-}
 pub const memory_pool = @import("heap/memory_pool.zig");
 
-/// Deprecated; use `memory_pool.Aligned`.
-pub const MemoryPoolAligned = memory_pool.Aligned;
-/// Deprecated; use `memory_pool.Extra`.
-pub const MemoryPoolExtra = memory_pool.Extra;
-/// Deprecated; use `memory_pool.Options`.
-pub const MemoryPoolOptions = memory_pool.Options;
+pub const MemoryPool = memory_pool.MemoryPool;
 
 /// comptime-known minimum page size of the target.
 ///
