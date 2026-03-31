d689367ed9ae5397a767c449b504177335cc9bb9
Jeff Anderson <kj4tmp@gmail.com>
2026-03-31 01:00:09 -0700

    port tools/update_openbsd_libc.zig

--- tools/update_openbsd_libc.zig
+++ tools/update_openbsd_libc.zig
@@ -1,9 +1,3 @@
-//! This script updates the .c, .h, .s, and .S files that make up the start
-//! files such as crt1.o.
-//!
-//! Example usage:
-//! `zig run tools/update_openbsd_libc.zig -- ~/Downloads/openbsd-src .`
-
 const std = @import("std");
 const Io = std.Io;
 
@@ -12,13 +6,38 @@ const exempt_files = [_][]const u8{
     "abilists",
 };
 
+const usage =
+    \\This script updates the .c, .h, .s, and .S files that make up the start
+    \\files such as crt1.o.
+    \\
+    \\Example usage:
+    \\`zig run tools/update_openbsd_libc.zig -- ~/Downloads/openbsd-src .`
+    \\
+;
+
+const command: std.cli.Command = .{
+    .name = "update_openbsd_libc",
+    .help = usage,
+    .positional_args = &.{
+        .init([:0]const u8, .{ .name = "openbsd_src_path" }),
+        .init([:0]const u8, .{ .name = "zig_src_path" }),
+    },
+};
+
 pub fn main(init: std.process.Init) !void {
     const arena = init.arena.allocator();
     const io = init.io;
     const args = try init.minimal.args.toSlice(arena);
 
-    const openbsd_src_path = args[1];
-    const zig_src_path = args[2];
+    const parsed = try std.cli.parse(command, arena, args, .{
+        .exit_help = true,
+        .exit_usage_error = true,
+        .render_usage_errors = true,
+        .render_help = true,
+    });
+
+    const openbsd_src_path = parsed.kind.args.openbsd_src_path;
+    const zig_src_path = parsed.kind.args.zig_src_path;
 
     const dest_dir_path = try std.fmt.allocPrint(arena, "{s}/lib/libc/openbsd", .{zig_src_path});
 
