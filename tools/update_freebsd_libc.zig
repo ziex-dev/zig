2604f0ebbfce5f7ac2d2a60bcbdd0a523db01385
Jeff Anderson <kj4tmp@gmail.com>
2026-03-31 00:46:48 -0700

    port tools/update_freebsd_libc.zig

--- tools/update_freebsd_libc.zig
+++ tools/update_freebsd_libc.zig
@@ -1,9 +1,3 @@
-//! This script updates the .c, .h, .s, and .S files that make up the start
-//! files such as crt1.o.
-//!
-//! Example usage:
-//! `zig run tools/update_freebsd_libc.zig -- ~/Downloads/freebsd-src .`
-
 const std = @import("std");
 const Io = std.Io;
 
@@ -12,13 +6,38 @@ const exempt_files = [_][]const u8{
     "abilists",
 };
 
+const usage =
+    \\usage: update_freebsd_libc -- path/downloaded/freebsd-src path/repo/zig
+    \\
+    \\This script updates the .c, .h, .s, and .S files that make up the start
+    \\files such as crt1.o.
+    \\
+    \\example: `zig run tools/update_freebsd_libc.zig -- ~/Downloads/freebsd-src .`
+    \\
+;
+const command: std.cli.Command = .{
+    .name = "update_freebsd_libc",
+    .help = usage,
+    .positional_args = &.{
+        .init([:0]const u8, .{ .name = "freebsd_src_path" }),
+        .init([:0]const u8, .{ .name = "zig_src_path" }),
+    },
+};
+
 pub fn main(init: std.process.Init) !void {
     const arena = init.arena.allocator();
     const io = init.io;
     const args = try init.minimal.args.toSlice(arena);
 
-    const freebsd_src_path = args[1];
-    const zig_src_path = args[2];
+    const parsed = try std.cli.parse(command, arena, args, .{
+        .exit_help = true,
+        .exit_usage_error = true,
+        .render_usage_errors = true,
+        .render_help = true,
+    });
+
+    const freebsd_src_path = parsed.kind.args.freebsd_src_path;
+    const zig_src_path = parsed.kind.args.zig_src_path;
 
     const dest_dir_path = try std.fmt.allocPrint(arena, "{s}/lib/libc/freebsd", .{zig_src_path});
 
