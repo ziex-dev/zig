394db20bbf988873cbe1869139bcccaebce8a027
Julian Dax <julian.dax@posteo.de>
2026-04-12 11:30:07 +0200

    fix Apple compiler detection regression

--- lib/compiler/aro/aro/Compilation.zig
+++ lib/compiler/aro/aro/Compilation.zig
@@ -376,6 +376,19 @@ fn generateSystemDefines(comp: *Compilation, w: *Io.Writer) !void {
         => {
             try define(w, "__APPLE__");
             try w.writeAll("#define __APPLE_CC__ 6000\n");
+            try define(w, "TARGET_OS_MAC");
+
+            const target_str = switch (target.os.tag) {
+                .maccatalyst => "TARGET_OS_MACCATALYST",
+                .macos => "TARGET_OS_OSX",
+                .tvos => "TARGET_OS_TV",
+                .ios => "TARGET_OS_IOS",
+                .driverkit => "TARGET_OS_DRIVERKIT",
+                .visionos => "TARGET_OS_VISION",
+                .watchos => "TARGET_OS_WATCH",
+                else => unreachable,
+            };
+            try define(w, target_str);
 
             const version = target.os.version_range.semver.min;
             var version_buf: [8]u8 = undefined;
@@ -445,6 +458,9 @@ fn generateSystemDefines(comp: *Compilation, w: *Io.Writer) !void {
                         \\#define _M_AMD64 100
                         \\
                     );
+                }
+                if (target.vendor == .apple) {
+                    try define(w, "TARGET_CPU_X86_64");
                 }
             } else {
                 try defineStd(w, "i386", is_gnu);
@@ -690,6 +706,9 @@ fn generateSystemDefines(comp: *Compilation, w: *Io.Writer) !void {
         },
         .aarch64, .aarch64_be => |arch| {
             try define(w, "__aarch64__");
+            if (target.vendor == .apple) {
+                try define(w,"TARGET_CPU_ARM64");
+            }
             switch (arch) {
                 .aarch64 => {
                     try define(w, "__AARCH64EL__");
