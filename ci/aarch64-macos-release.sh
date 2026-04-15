855cb2cc1749ba3b683ad31205e6b451d34a7ddd
Andrew Kelley <andrew@ziglang.org>
2026-04-15 14:18:56 -0700

    CI: add coverage for fuzzer compiling
    From mlugg: this commit originally also added the same coverage to the
    aarch64-linux-release script, but that caused the aarch64-linux-release
    job to time out, so that change has been omitted for now.

--- ci/aarch64-macos-release.sh
+++ ci/aarch64-macos-release.sh
@@ -51,6 +51,10 @@ stage3-release/bin/zig build test docs \
   --search-prefix "$PREFIX" \
   --test-timeout 2m
 
+# Ensure that the fuzzer at least compiles.
+stage3-release/bin/zig build test-std --fuzz=1K -Dno-lib -Dfuzz-only -Doptimize=ReleaseSafe
+stage3-release/bin/zig build test-std --fuzz=1K -Dno-lib -Dfuzz-only -Doptimize=Debug
+
 # Ensure that stage3 and stage4 are byte-for-byte identical.
 stage3-release/bin/zig build \
   --maxrss ${ZSF_MAX_RSS:-0} \
