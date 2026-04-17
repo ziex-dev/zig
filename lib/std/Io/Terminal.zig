decb2f0e2d0e41d85f5f8db2f0f24c9f095d0c8b
Carter Snook <cartersnook04@gmail.com>
2026-04-17 13:26:37 -0500

    expose Terminal.setColor() behavior in Terminal.Mode
    Terminal offers extremely useful functionality! However, there are many
    times where you already have a writer lying around (e.g. format()
    method). Therefore, this makes it so that you don't need to create a
    temporary terminal in those instances, similar to the previous version
    of std.io.tty.Config.
    
    This feels along the same lines as making std.ArrayList() and friends
    not hold the allocator directly.

--- lib/std/Io/Terminal.zig
+++ lib/std/Io/Terminal.zig
@@ -78,67 +78,124 @@ pub const Mode = union(enum) {
         }
         return if (force_color == true) .escape_codes else .no_color;
     }
+
+    pub const SetColorError = Io.Cancelable || Io.UnexpectedError || Io.Writer.Error;
+
+    pub fn setColor(mode: Mode, writer: *std.Io.Writer, color: Color) Mode.SetColorError!void {
+        switch (mode) {
+            .no_color => return,
+            .escape_codes => {
+                const color_string = switch (color) {
+                    .black => "\x1b[30m",
+                    .red => "\x1b[31m",
+                    .green => "\x1b[32m",
+                    .yellow => "\x1b[33m",
+                    .blue => "\x1b[34m",
+                    .magenta => "\x1b[35m",
+                    .cyan => "\x1b[36m",
+                    .white => "\x1b[37m",
+                    .bright_black => "\x1b[90m",
+                    .bright_red => "\x1b[91m",
+                    .bright_green => "\x1b[92m",
+                    .bright_yellow => "\x1b[93m",
+                    .bright_blue => "\x1b[94m",
+                    .bright_magenta => "\x1b[95m",
+                    .bright_cyan => "\x1b[96m",
+                    .bright_white => "\x1b[97m",
+                    .bold => "\x1b[1m",
+                    .dim => "\x1b[2m",
+                    .reset => "\x1b[0m",
+                };
+                try writer.writeAll(color_string);
+            },
+            .windows_api => |wa| {
+                const windows = std.os.windows;
+                const attributes: windows.WORD = switch (color) {
+                    .black => 0,
+                    .red => windows.FOREGROUND_RED,
+                    .green => windows.FOREGROUND_GREEN,
+                    .yellow => windows.FOREGROUND_RED | windows.FOREGROUND_GREEN,
+                    .blue => windows.FOREGROUND_BLUE,
+                    .magenta => windows.FOREGROUND_RED | windows.FOREGROUND_BLUE,
+                    .cyan => windows.FOREGROUND_GREEN | windows.FOREGROUND_BLUE,
+                    .white => windows.FOREGROUND_RED | windows.FOREGROUND_GREEN | windows.FOREGROUND_BLUE,
+                    .bright_black => windows.FOREGROUND_INTENSITY,
+                    .bright_red => windows.FOREGROUND_RED | windows.FOREGROUND_INTENSITY,
+                    .bright_green => windows.FOREGROUND_GREEN | windows.FOREGROUND_INTENSITY,
+                    .bright_yellow => windows.FOREGROUND_RED | windows.FOREGROUND_GREEN | windows.FOREGROUND_INTENSITY,
+                    .bright_blue => windows.FOREGROUND_BLUE | windows.FOREGROUND_INTENSITY,
+                    .bright_magenta => windows.FOREGROUND_RED | windows.FOREGROUND_BLUE | windows.FOREGROUND_INTENSITY,
+                    .bright_cyan => windows.FOREGROUND_GREEN | windows.FOREGROUND_BLUE | windows.FOREGROUND_INTENSITY,
+                    .bright_white, .bold => windows.FOREGROUND_RED | windows.FOREGROUND_GREEN | windows.FOREGROUND_BLUE | windows.FOREGROUND_INTENSITY,
+                    // "dim" is not supported using basic character attributes, but let's still make it do *something*.
+                    // This matches the old behavior of TTY.Color before the bright variants were added.
+                    .dim => windows.FOREGROUND_INTENSITY,
+                    .reset => wa.reset_attributes,
+                };
+                try writer.flush();
+                var set_text_attribute = windows.CONSOLE.USER_IO.SET_TEXT_ATTRIBUTE(attributes);
+                switch (try set_text_attribute.operate(wa.io, wa.file)) {
+                    .SUCCESS => {},
+                    else => |status| return windows.unexpectedStatus(status),
+                }
+            },
+        }
+    }
 };
 
-pub const SetColorError = Io.Cancelable || Io.UnexpectedError || Io.Writer.Error;
+pub const SetColorError = Mode.SetColorError;
 
+/// Equivalent to calling `t.mode.setColor(t.writer, color)`.
 pub fn setColor(t: Terminal, color: Color) SetColorError!void {
-    switch (t.mode) {
-        .no_color => return,
-        .escape_codes => {
-            const color_string = switch (color) {
-                .black => "\x1b[30m",
-                .red => "\x1b[31m",
-                .green => "\x1b[32m",
-                .yellow => "\x1b[33m",
-                .blue => "\x1b[34m",
-                .magenta => "\x1b[35m",
-                .cyan => "\x1b[36m",
-                .white => "\x1b[37m",
-                .bright_black => "\x1b[90m",
-                .bright_red => "\x1b[91m",
-                .bright_green => "\x1b[92m",
-                .bright_yellow => "\x1b[93m",
-                .bright_blue => "\x1b[94m",
-                .bright_magenta => "\x1b[95m",
-                .bright_cyan => "\x1b[96m",
-                .bright_white => "\x1b[97m",
-                .bold => "\x1b[1m",
-                .dim => "\x1b[2m",
-                .reset => "\x1b[0m",
-            };
-            try t.writer.writeAll(color_string);
-        },
-        .windows_api => |wa| {
-            const windows = std.os.windows;
-            const attributes: windows.WORD = switch (color) {
-                .black => 0,
-                .red => windows.FOREGROUND_RED,
-                .green => windows.FOREGROUND_GREEN,
-                .yellow => windows.FOREGROUND_RED | windows.FOREGROUND_GREEN,
-                .blue => windows.FOREGROUND_BLUE,
-                .magenta => windows.FOREGROUND_RED | windows.FOREGROUND_BLUE,
-                .cyan => windows.FOREGROUND_GREEN | windows.FOREGROUND_BLUE,
-                .white => windows.FOREGROUND_RED | windows.FOREGROUND_GREEN | windows.FOREGROUND_BLUE,
-                .bright_black => windows.FOREGROUND_INTENSITY,
-                .bright_red => windows.FOREGROUND_RED | windows.FOREGROUND_INTENSITY,
-                .bright_green => windows.FOREGROUND_GREEN | windows.FOREGROUND_INTENSITY,
-                .bright_yellow => windows.FOREGROUND_RED | windows.FOREGROUND_GREEN | windows.FOREGROUND_INTENSITY,
-                .bright_blue => windows.FOREGROUND_BLUE | windows.FOREGROUND_INTENSITY,
-                .bright_magenta => windows.FOREGROUND_RED | windows.FOREGROUND_BLUE | windows.FOREGROUND_INTENSITY,
-                .bright_cyan => windows.FOREGROUND_GREEN | windows.FOREGROUND_BLUE | windows.FOREGROUND_INTENSITY,
-                .bright_white, .bold => windows.FOREGROUND_RED | windows.FOREGROUND_GREEN | windows.FOREGROUND_BLUE | windows.FOREGROUND_INTENSITY,
-                // "dim" is not supported using basic character attributes, but let's still make it do *something*.
-                // This matches the old behavior of TTY.Color before the bright variants were added.
-                .dim => windows.FOREGROUND_INTENSITY,
-                .reset => wa.reset_attributes,
-            };
-            try t.writer.flush();
-            var set_text_attribute = windows.CONSOLE.USER_IO.SET_TEXT_ATTRIBUTE(attributes);
-            switch (try set_text_attribute.operate(wa.io, wa.file)) {
-                .SUCCESS => {},
-                else => |status| return windows.unexpectedStatus(status),
-            }
-        },
-    }
+    try t.mode.setColor(t.writer, color);
+}
+
+test "write terminal message with escape codes" {
+    var terminal_writer: std.Io.Writer.Allocating = .init(std.testing.allocator);
+    defer terminal_writer.deinit();
+
+    const terminal: Terminal = .{
+        .writer = &terminal_writer.writer,
+        .mode = .escape_codes,
+    };
+
+    try terminal.setColor(.red);
+    try terminal.setColor(.bold);
+    try terminal.writer.writeAll("Hello ");
+    try terminal.setColor(.reset);
+
+    try terminal.setColor(.bright_green);
+    try terminal.setColor(.dim);
+    try terminal.writer.writeAll("there.");
+    try terminal.setColor(.reset);
+
+    try std.testing.expectEqualStrings(
+        "\x1b[31m\x1b[1mHello \x1b[0m\x1b[92m\x1b[2mthere.\x1b[0m",
+        terminal_writer.written(),
+    );
+}
+
+test "write terminal message with no color" {
+    var terminal_writer: std.Io.Writer.Allocating = .init(std.testing.allocator);
+    defer terminal_writer.deinit();
+
+    const terminal: Terminal = .{
+        .writer = &terminal_writer.writer,
+        .mode = .no_color,
+    };
+
+    try terminal.setColor(.red);
+    try terminal.setColor(.bold);
+    try terminal.writer.writeAll("Hello ");
+    try terminal.setColor(.reset);
+
+    try terminal.setColor(.bright_green);
+    try terminal.setColor(.dim);
+    try terminal.writer.writeAll("there.");
+    try terminal.setColor(.reset);
+
+    try std.testing.expectEqualStrings(
+        "Hello there.",
+        terminal_writer.written(),
+    );
 }
