0928197f3676e186c80ab55223f606672fb82467
Chris Boesch <chrboesch@noreply.codeberg.org>
2026-04-23 00:56:17 +0200

    finished l64a

--- lib/c/misc.zig
+++ lib/c/misc.zig
@@ -17,13 +17,13 @@ fn a64l(str: [*c]const u8) c_long {
     const ptr: [*:0]const u8 = @ptrCast(str);
 
     var x: u32 = 0;
-    var e: u5 = 0;
+    var e: u32 = 0;
     var n: usize = 0;
     while (n < 6) : (n += 1) {
         const chr = ptr[n];
         if (chr == 0) break;
         const idx = std.mem.indexOfScalar(u8, digits, chr) orelse break;
-        x |= @as(u32, @intCast(idx)) << e;
+        x |= @as(u32, @intCast(idx)) << @intCast(e);
         e += 6;
     }
 
@@ -31,20 +31,18 @@ fn a64l(str: [*c]const u8) c_long {
 }
 
 fn l64a(x0: c_long) [*c]u8 {
-    _ = x0;
-    return 0;
-}
+    const static = struct {
+        var str: [7]u8 = undefined;
+    };
 
-const digit_to_value: [256]u8 = blk: {
-    var table: [256]u8 = undefined;
-    @memset(&table, 0xFF);
-    for (digits, 0..) |d, i| {
-        table[d] = @intCast(i);
+    var x: u32 = @truncate(@as(c_ulong, @bitCast(x0)));
+    var n: usize = 0;
+    while (n < 6) : (n += 1) {
+        if (x == 0) break;
+        static.str[n] = digits[x & 63];
+        x >>= 6;
     }
-    break :blk table;
-};
 
-// test "a64l" {
-//     try std.testing.expectEqual(a64l(null), 0);
-//     try std.testing.expectEqual(a64l("v/"), 123);
-// }
+    static.str[n] = 0;
+    return &static.str;
+}
--- test/c/misc.zig
+++ test/c/misc.zig
@@ -6,6 +6,20 @@ const testing = std.testing;
 
 test "a64l" {
     if (builtin.target.os.tag == .windows) return; // no a64l
-    try std.testing.expectEqual(c.a64l(null), 0);
-    try std.testing.expectEqual(c.a64l("v/"), 123);
+    try testing.expectEqual(123, c.a64l("v/"));
+    try testing.expectEqual(-1, c.a64l("zzzzzz"));
+    try testing.expectEqual(-262021, c.a64l("v/.zzzz"));
+    try testing.expectEqual(0, c.a64l("."));
+    try testing.expectEqual(1, c.a64l("/"));
+    try testing.expectEqual(1, c.a64l("/."));
+}
+
+test "l64a" {
+    if (builtin.target.os.tag == .windows) return; // no l64a
+    try testing.expectEqualStrings("", std.mem.span(c.l64a(0)));
+    try testing.expectEqualStrings("v/", std.mem.span(c.l64a(123)));
+    try testing.expectEqualStrings("/", std.mem.span(c.l64a(1)));
+    try testing.expectEqualStrings("./", std.mem.span(c.l64a(64)));
+    try testing.expectEqualStrings("zzzzz1", std.mem.span(c.l64a(-1)));
+    try testing.expectEqualStrings("v/.zz1", std.mem.span(c.l64a(-262021)));
 }
