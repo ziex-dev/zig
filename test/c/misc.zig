f03f0d9731bc8e5ee67e4be10d9b4ccd5b82d832
Chris Boesch <chrboesch@noreply.codeberg.org>
2026-04-24 00:19:46 +0200

    added casts to tests

--- test/c/misc.zig
+++ test/c/misc.zig
@@ -6,20 +6,20 @@ const testing = std.testing;
 
 test "a64l" {
     if (builtin.target.os.tag == .windows) return; // no a64l
-    try testing.expectEqual(123, c.a64l("v/"));
-    try testing.expectEqual(-1, c.a64l("zzzzzz"));
-    try testing.expectEqual(-262021, c.a64l("v/.zzzz"));
-    try testing.expectEqual(0, c.a64l("."));
-    try testing.expectEqual(1, c.a64l("/"));
-    try testing.expectEqual(1, c.a64l("/."));
+    try testing.expectEqual(123, c.a64l(@ptrCast("v/")));
+    try testing.expectEqual(-1, c.a64l(@ptrCast("zzzzzz")));
+    try testing.expectEqual(-262021, c.a64l(@ptrCast("v/.zzzz")));
+    try testing.expectEqual(0, c.a64l(@ptrCast(".")));
+    try testing.expectEqual(1, c.a64l(@ptrCast("/")));
+    try testing.expectEqual(1, c.a64l(@ptrCast("/.")));
 }
 
 test "l64a" {
     if (builtin.target.os.tag == .windows) return; // no l64a
-    try testing.expectEqualStrings("", std.mem.span(c.l64a(0)));
-    try testing.expectEqualStrings("v/", std.mem.span(c.l64a(123)));
-    try testing.expectEqualStrings("/", std.mem.span(c.l64a(1)));
-    try testing.expectEqualStrings("./", std.mem.span(c.l64a(64)));
-    try testing.expectEqualStrings("zzzzz1", std.mem.span(c.l64a(-1)));
-    try testing.expectEqualStrings("v/.zz1", std.mem.span(c.l64a(-262021)));
+    try testing.expectEqualStrings("", std.mem.span(@as([*:0]const u8, c.l64a(0))));
+    try testing.expectEqualStrings("v/", std.mem.span(@as([*:0]const u8, c.l64a(123))));
+    try testing.expectEqualStrings("/", std.mem.span(@as([*:0]const u8, c.l64a(1))));
+    try testing.expectEqualStrings("./", std.mem.span(@as([*:0]const u8, c.l64a(64))));
+    try testing.expectEqualStrings("zzzzz1", std.mem.span(@as([*:0]const u8, c.l64a(-1))));
+    try testing.expectEqualStrings("v/.zz1", std.mem.span(@as([*:0]const u8, c.l64a(-262021))));
 }
