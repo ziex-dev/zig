db745bc19120e9a0f49193fce83f219fef40d325
Chris Boesch <chrboesch@noreply.codeberg.org>
2026-04-24 11:37:36 +0200

    skipped tests with -1 for gnu

--- test/c/misc.zig
+++ test/c/misc.zig
@@ -7,11 +7,12 @@ const testing = std.testing;
 test "a64l" {
     if (builtin.target.os.tag == .windows) return; // no a64l
     try testing.expectEqual(123, c.a64l(@ptrCast("v/")));
-    try testing.expectEqual(-1, c.a64l(@ptrCast("zzzzzz")));
-    try testing.expectEqual(-262021, c.a64l(@ptrCast("v/.zzzz")));
     try testing.expectEqual(0, c.a64l(@ptrCast(".")));
     try testing.expectEqual(1, c.a64l(@ptrCast("/")));
     try testing.expectEqual(1, c.a64l(@ptrCast("/.")));
+    if (!builtin.target.abi.isGnu()) return; // no negativ values tests
+    try testing.expectEqual(-1, c.a64l(@ptrCast("zzzzzz")));
+    try testing.expectEqual(-262021, c.a64l(@ptrCast("v/.zzzz")));
 }
 
 test "l64a" {
@@ -20,6 +21,7 @@ test "l64a" {
     try testing.expectEqualStrings("v/", std.mem.span(@as([*:0]const u8, c.l64a(123))));
     try testing.expectEqualStrings("/", std.mem.span(@as([*:0]const u8, c.l64a(1))));
     try testing.expectEqualStrings("./", std.mem.span(@as([*:0]const u8, c.l64a(64))));
+    if (!builtin.target.abi.isGnu()) return; // no negativ values tests
     try testing.expectEqualStrings("zzzzz1", std.mem.span(@as([*:0]const u8, c.l64a(-1))));
     try testing.expectEqualStrings("v/.zz1", std.mem.span(@as([*:0]const u8, c.l64a(-262021))));
 }
