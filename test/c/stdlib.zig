708c639c85bd7aa1252cba36afcb3da80edc7882
Chris Boesch <chrboesch@noreply.codeberg.org>
2026-04-26 00:15:50 +0200

    moved tests to stdlib too

--- test/c/stdlib.zig
+++ test/c/stdlib.zig
@@ -376,3 +376,21 @@ test "bsearch" {
 test {
     _ = @import("stdlib/drand48.zig");
 }
+
+test "a64l" {
+    if (builtin.target.os.tag == .windows) return; // no a64l
+    try testing.expectEqual(183231, c.a64l(@ptrCast("zig")));
+    try testing.expectEqual(123, c.a64l(@ptrCast("v/")));
+    try testing.expectEqual(0, c.a64l(@ptrCast(".")));
+    try testing.expectEqual(1, c.a64l(@ptrCast("/")));
+    try testing.expectEqual(1, c.a64l(@ptrCast("/.")));
+}
+
+test "l64a" {
+    if (builtin.target.os.tag == .windows) return; // no a64l
+    try testing.expectEqualStrings("zig", std.mem.span(@as([*:0]const u8, c.l64a(183231))));
+    try testing.expectEqualStrings("", std.mem.span(@as([*:0]const u8, c.l64a(0))));
+    try testing.expectEqualStrings("v/", std.mem.span(@as([*:0]const u8, c.l64a(123))));
+    try testing.expectEqualStrings("/", std.mem.span(@as([*:0]const u8, c.l64a(1))));
+    try testing.expectEqualStrings("./", std.mem.span(@as([*:0]const u8, c.l64a(64))));
+}
