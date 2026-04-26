3cf0fe22649103b45bbaf08cbf131f712f31e834
Chris Boesch <chrboesch@noreply.codeberg.org>
2026-04-26 19:57:16 +0200

    removed misc.zig again

--- test/c.zig
+++ test/c.zig
@@ -5,7 +5,6 @@ const c = std.c;
 test {
     _ = @import("c/inttypes.zig");
     _ = @import("c/math.zig");
-    _ = @import("c/misc.zig");
     _ = @import("c/pthread.zig");
     _ = @import("c/search.zig");
     _ = @import("c/stdlib.zig");
