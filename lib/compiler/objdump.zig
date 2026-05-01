c6b9835b4d68f5daecfb6616996a4a1c5610d380
Simon Hartcher <simon@simonhartcher.com>
2026-05-01 12:49:43 +1000

    zig objdump: remove input path from elf output
    Removes `Config` as not needed.

--- lib/compiler/objdump.zig
+++ lib/compiler/objdump.zig
@@ -3,13 +3,9 @@ const Io = std.Io;
 const fatal = std.process.fatal;
 const mem = std.mem;
 const assert = std.debug.assert;
+const Allocator = std.mem.Allocator;
 
 var stdout_buffer: [4000]u8 = undefined;
-
-const Config = struct {
-    allocator: std.mem.Allocator,
-    input_path: []const u8,
-};
 
 pub fn main(init: std.process.Init) !void {
     const io = init.io;
@@ -38,15 +34,10 @@ pub fn main(init: std.process.Init) !void {
         fatal("failed to open {s}: {t}", .{ input_path, err });
     defer file.close(io);
 
-    const config: Config = .{
-        .allocator = init.arena.allocator(),
-        .input_path = input_path,
-    };
-
     var buffer: [4000]u8 = undefined;
     var file_reader = file.reader(io, &buffer);
     var stdout_writer = std.Io.File.stdout().writerStreaming(io, &stdout_buffer);
-    dump(&file_reader.interface, &stdout_writer.interface, config) catch |err| switch (err) {
+    dump(init.arena.allocator(), &file_reader.interface, &stdout_writer.interface) catch |err| switch (err) {
         error.ReadFailed => return file_reader.err.?,
         error.WriteFailed => return stdout_writer.err.?,
         error.UnknownFile => fatal("unrecognized file: {s}", .{input_path}),
@@ -55,31 +46,30 @@ pub fn main(init: std.process.Init) !void {
     try stdout_writer.flush();
 }
 
-fn dump(r: *Io.Reader, w: *Io.Writer, c: Config) !void {
+fn dump(arena: Allocator, r: *Io.Reader, w: *Io.Writer) !void {
     try r.fill(4);
     elf: {
         if (!mem.eql(u8, r.buffered()[0..4], std.elf.MAGIC)) break :elf;
-        return elf.dump(r, w, c);
+        return elf.dump(arena, r, w);
     }
     macho: {
         if (mem.readInt(u32, r.buffered()[0..4], .little) != std.macho.MH_MAGIC_64) break :macho;
-        return macho.dump(r, w, c);
+        return macho.dump(r, w);
     }
     wasm: {
         comptime assert(std.wasm.magic.len == 4);
         if (!mem.eql(u8, r.buffered()[0..4], &std.wasm.magic)) break :wasm;
-        return wasm.dump(r, w, c);
+        return wasm.dump(r, w);
     }
     return error.UnknownFile;
 }
 
 const elf = struct {
-    fn dump(r: *Io.Reader, w: *Io.Writer, c: Config) !void {
+    fn dump(arena: Allocator, r: *Io.Reader, w: *Io.Writer) !void {
         const h: std.elf.Header = try .read(r);
 
         try w.print(
-            \\{s}: file format {s}
-            \\
+            \\File format: {s}
             \\Binary type: {s}
             \\Machine:     {s}
             \\Entry point: 0x{x:0>16}
@@ -88,8 +78,7 @@ const elf = struct {
             \\OS ABI:      {s}
             \\
         , .{
-            c.input_path,
-            try h.targetName(c.allocator),
+            try h.targetName(arena),
             @tagName(h.type),
             @tagName(h.machine),
             h.entry,
@@ -101,14 +90,14 @@ const elf = struct {
 };
 
 const macho = struct {
-    fn dump(r: *Io.Reader, w: *Io.Writer, _: Config) !void {
+    fn dump(r: *Io.Reader, w: *Io.Writer) !void {
         _ = r;
         try w.writeAll("TODO dump macho file\n");
     }
 };
 
 const wasm = struct {
-    fn dump(r: *Io.Reader, w: *Io.Writer, _: Config) !void {
+    fn dump(r: *Io.Reader, w: *Io.Writer) !void {
         _ = r;
         try w.writeAll("TODO dump wasm file\n");
     }
