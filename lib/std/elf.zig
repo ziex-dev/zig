6ec929c0ff31e622252c2d0733ce2c8e156de5fc
Simon Hartcher <simon@simonhartcher.com>
2026-05-01 12:07:11 +1000

    std.elf.Header: cleanup and doc comment
    Rename targetName allocator argument to arena that indicates to the caller that this function leaks.
    Explain the return format in doc comment.

--- lib/std/elf.zig
+++ lib/std/elf.zig
@@ -835,24 +835,32 @@ pub const Header = struct {
         };
     }
 
-    pub fn targetName(h: Header, allocator: std.mem.Allocator) ![]const u8 {
-        const prefix = if (h.is_64) "elf64" else "elf32";
+    /// A short name that describes the ELF target.
+    ///
+    /// Format: elf{32|64}-{arch}
+    ///     - `elf32` / `elf64`: ELF class
+    ///     - BFD-inspired architecture identifier encoding endianness
+    ///       E.g.: `littleaarch64`, `powerpcle`
+    ///
+    /// These are analogous to `TARGET_{BIG|LITTLE}_NAME` in binutils.
+    pub fn targetName(h: Header, arena: std.mem.Allocator) ![]const u8 {
+        const prefix = if (h.is_64) "64" else "32";
         const suffix = switch (h.machine) {
             .X86_64 => "x86-64",
             .@"386" => "i386",
             .AARCH64 => if (h.endian == .little) "littleaarch64" else "bigaarch64",
             .ARM => if (h.endian == .little) "littlearm" else "bigarm",
-            .RISCV => "littleriscv",
+            .RISCV => if (h.endian == .little) "littleriscv" else "bigriscv",
             .PPC64 => if (h.endian == .little) "powerpcle" else "powerpc",
             .PPC => if (h.endian == .little) "powerpcle" else "powerpc",
-            .MIPS => if (h.endian == .little) "tradlittlemips" else "tradbigmips",
+            .MIPS => if (h.endian == .little) "littlemips" else "bigmips",
             .LOONGARCH => "loongarch",
             .S390 => "s390",
             .SPARCV9 => "sparc",
             else => "unknown",
         };
 
-        return std.fmt.allocPrint(allocator, "{s}-{s}", .{ prefix, suffix });
+        return std.fmt.allocPrint(arena, "elf{s}-{s}", .{ prefix, suffix });
     }
 };
 
