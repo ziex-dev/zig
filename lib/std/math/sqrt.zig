5c74cda6a29f59416be8e1c05adc1c8244ceb933
Hila Friedman <red.black.liquorice@gmail.com>
2026-05-10 20:03:18 +0300

    make std.math.sqrt to work on big comptime_ints

--- lib/std/math/sqrt.zig
+++ lib/std/math/sqrt.zig
@@ -17,29 +17,23 @@ pub fn sqrt(x: anytype) Sqrt(@TypeOf(x)) {
     switch (@typeInfo(T)) {
         .float, .comptime_float => return @sqrt(x),
         .comptime_int => comptime {
-            if (x > maxInt(u128)) {
-                @compileError("sqrt not implemented for comptime_int greater than 128 bits");
-            }
-            if (x < 0) {
-                @compileError("sqrt on negative number");
-            }
-            return @as(T, sqrt_int(u128, x));
+            if (x < 0) @compileError("sqrt on negative number");
+            return sqrtInt(math.IntFittingRange(0, x), x);
         },
         .int => |IntType| switch (IntType.signedness) {
             .signed => @compileError("sqrt not implemented for signed integers"),
-            .unsigned => return sqrt_int(T, x),
+            .unsigned => return sqrtInt(T, x),
         },
         else => @compileError("sqrt not implemented for " ++ @typeName(T)),
     }
 }
 
-fn sqrt_int(comptime T: type, value: T) Sqrt(T) {
+fn sqrtInt(comptime T: type, value: T) Sqrt(T) {
     if (@typeInfo(T).int.bits <= 2) {
         return if (value == 0) 0 else 1; // shortcut for small number of bits to simplify general case
     } else {
         const bits = @typeInfo(T).int.bits;
-        const max = math.maxInt(T);
-        const minustwo = (@as(T, 2) ^ max) + 1; // unsigned int cannot represent -2
+        const minustwo = @as(T, 0) -% 2; // unsigned int cannot represent -2
         var op = value;
         var res: T = 0;
         var one: T = 1 << ((bits - 1) & minustwo); // highest power of four that fits into T
@@ -57,24 +51,24 @@ fn sqrt_int(comptime T: type, value: T) Sqrt(T) {
             one >>= 2;
         }
 
-        return @as(Sqrt(T), @intCast(res));
+        return @intCast(res);
     }
 }
 
-test sqrt_int {
-    try expect(sqrt_int(u32, 3) == 1);
-    try expect(sqrt_int(u32, 4) == 2);
-    try expect(sqrt_int(u32, 5) == 2);
-    try expect(sqrt_int(u32, 8) == 2);
-    try expect(sqrt_int(u32, 9) == 3);
-    try expect(sqrt_int(u32, 10) == 3);
+test sqrtInt {
+    try expect(sqrtInt(u32, 3) == 1);
+    try expect(sqrtInt(u32, 4) == 2);
+    try expect(sqrtInt(u32, 5) == 2);
+    try expect(sqrtInt(u32, 8) == 2);
+    try expect(sqrtInt(u32, 9) == 3);
+    try expect(sqrtInt(u32, 10) == 3);
 
-    try expect(sqrt_int(u0, 0) == 0);
-    try expect(sqrt_int(u1, 1) == 1);
-    try expect(sqrt_int(u2, 3) == 1);
-    try expect(sqrt_int(u3, 4) == 2);
-    try expect(sqrt_int(u4, 8) == 2);
-    try expect(sqrt_int(u4, 9) == 3);
+    try expect(sqrtInt(u0, 0) == 0);
+    try expect(sqrtInt(u1, 1) == 1);
+    try expect(sqrtInt(u2, 3) == 1);
+    try expect(sqrtInt(u3, 4) == 2);
+    try expect(sqrtInt(u4, 8) == 2);
+    try expect(sqrtInt(u4, 9) == 3);
 }
 
 /// Returns the return type `sqrt` will return given an operand of type `T`.
