33e84a6212ae4dd9ef0a0fdbd9dbd3d3644c77da
Brandon Black <bblack@wikimedia.org>
2026-05-11 07:34:52 -0500

    Io.operateTimeout: if timeout == .none, operate
    It's far simpler for the same effective outcome (no Batch
    instantiation, no quick-success attempt + infinite poll first)

--- lib/std/Io.zig
+++ lib/std/Io.zig
@@ -466,6 +466,7 @@ pub const OperateTimeoutError = Cancelable || Timeout.Error || ConcurrentError;
 
 /// Performs one `Operation` with provided `timeout`.
 pub fn operateTimeout(io: Io, operation: Operation, timeout: Timeout) OperateTimeoutError!Operation.Result {
+    if (timeout == .none) return io.vtable.operate(io.userdata, operation);
     var storage: [1]Operation.Storage = undefined;
     var batch: Batch = .init(&storage);
     batch.addAt(0, operation);
