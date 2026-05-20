c271009542effd84e871b1aa6804e6a5a8cbf255
EJ <evyatar.shafran@gmail.com>
2026-05-20 18:14:14 +0300

    std.Io.Dir: readd test guard

--- lib/std/fs/test.zig
+++ lib/std/fs/test.zig
@@ -1094,6 +1094,11 @@ test "Dir.renamePreserve onto existing" {
             // file -> dir
             try expectError(error.PathAlreadyExists, ctx.dir.renamePreserve(test_file_path, ctx.dir, target_dir_path, io));
 
+            // TODO: fix dir renaming on other systems, see https://codeberg.org/ziglang/zig/issues/35340
+            if (native_os != .windows and native_os != .linux and !native_os.isDarwin()) {
+                return;
+            }
+
             // dir -> file
             try expectError(error.PathAlreadyExists, ctx.dir.renamePreserve(test_dir_path, ctx.dir, target_file_path, io));
             // dir -> dir
