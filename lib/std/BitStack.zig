5c2c9e92e941df7e06aa01cd9672bedf1cb2b518
Rue04 <rue.nolastname@protonmail.com>
2026-05-26 22:34:32 +0200

    `std.BitStack`: basic unmanaged implementation
    i kinda wanna add some more things to it, like being able to push multiple things at once,
    but also idk if that usecase is really necessary.. i suppose ill see when i change all
    the uses of it

--- lib/std/BitStack.zig
+++ lib/std/BitStack.zig
@@ -1,9 +1,141 @@
-//! Effectively a stack of u1 values implemented using ArrayList(u8).
+//! A packed stack of `u1`s.
+const BitStack = @This();
 
 const std = @import("std");
-const Allocator = std.mem.Allocator;
-const ArrayList = std.array_list.Managed;
 
+const Allocator = std.mem.Allocator;
+const assert = std.debug.assert;
+
+bytes: []u8,
+bit_len: usize,
+
+pub const empty: BitStack = .{
+    .bytes = &.{},
+    .bit_len = 0,
+};
+
+/// Returns capacity in bits.
+pub fn capacity(self: BitStack) usize {
+    return self.bytes.len * 8;
+}
+
+/// Initialize with capacity to hold exactly `num_bits`
+/// bits.
+/// Deinitialize with `deinit`.
+pub fn initCapacity(gpa: Allocator, num_bits: usize) Allocator.Error!BitStack {
+    var self: BitStack = .empty;
+    try self.ensureTotalCapacity(gpa, num_bits);
+    return self;
+}
+/// Initialize with externally-managed memory. The buffer
+/// determines the capacity.
+///
+/// When initialized this way, all functions that accept an Allocator
+/// argument cause illegal behavior.
+pub fn initBuffer(buffer: []u8) BitStack {
+    return .{
+        .bytes = buffer,
+        .bit_len = 0,
+    };
+}
+/// Creates a copy of `self`.
+/// Deinitialize with `deinit`.
+pub fn clone(self: BitStack, gpa: Allocator) Allocator.Error!BitStack {
+    var cloned = try initCapacity(gpa, self.capacity());
+    errdefer cloned.deinit(gpa);
+    const used_bytes = numBitsToNumBytes(self.bit_len);
+    @memcpy(cloned.bytes[0..used_bytes], self.bytes[0..used_bytes]);
+    cloned.bit_len = self.bit_len;
+    return cloned;
+}
+/// Release all allocated memory.
+pub fn deinit(self: *BitStack, gpa: Allocator) void {
+    gpa.free(self.bytes);
+    self.* = undefined;
+}
+
+/// If the current capacity is less than `num_bits`,
+/// expand capacity such that self can hold `num_bits` bits.
+/// Rounds the capacity to the next highest multiple of 8.
+pub fn ensureTotalCapacity(self: *BitStack, gpa: Allocator, num_bits: usize) Allocator.Error!void {
+    if (self.capacity() >= num_bits) return;
+
+    const num_bytes = numBitsToNumBytes(num_bits);
+    const old_bytes = self.bytes;
+    if (gpa.remap(old_bytes, num_bytes)) |new_bytes| {
+        self.bytes = new_bytes;
+    } else {
+        const new_bytes = gpa.alloc(u8, num_bytes);
+        @memcpy(new_bytes[0..old_bytes.len], old_bytes);
+        gpa.free(old_bytes);
+        self.bytes = new_bytes;
+    }
+}
+/// Expand capacity such that `self` can hold up to
+/// `additional_bits` more bits.
+pub fn ensureUnusedCapacity(self: *BitStack, gpa: Allocator, additional_bits: usize) Allocator.Error!void {
+    const num_bits = try addOrOom(self.bit_len, additional_bits);
+    return self.ensureTotalCapacity(gpa, num_bits);
+}
+
+pub fn push(self: *BitStack, gpa: Allocator, bit: u1) Allocator.Error!void {
+    try self.ensureUnusedCapacity(gpa, 1);
+    self.pushAssumeCapacity(bit);
+}
+/// If `self` cannot hold an additional bit, returns
+/// `error.OutOfMemory`.
+pub fn pushBounded(self: *BitStack, bit: u1) error{OutOfMemory}!void {
+    if (self.bit_len >= self.capacity()) return error.OutOfMemory;
+    self.pushAssumeCapacity(bit);
+}
+/// Asserts that `self` can old an additional bit.
+pub fn pushAssumeCapacity(self: *BitStack, bit: u1) void {
+    assert(self.bit_len < self.capacity());
+
+    const byte_idx, const bit_idx = bitIdxToByteIdx(self.bit_len);
+    self.bytes[byte_idx] &= ~(@as(u8, 1) << bit_idx);
+    self.bytes[byte_idx] |= @as(u8, bit) << bit_idx;
+    self.bit_len += 1;
+}
+
+/// Remove and return top-most bit from `self`.
+/// If `self` is empty, returns `null`.
+pub fn pop(self: *BitStack) ?u1 {
+    const bit = self.peek() orelse return null;
+    self.bit_len -= 1;
+    return bit;
+}
+/// Return top-most bit from `self`.
+/// If `self` is empty, returns `null`.
+pub fn peek(self: BitStack) ?u1 {
+    if (self.bit_len <= 0) return null;
+    const byte_idx, const bit_idx = bitIdxToByteIdx(self.bit_len - 1);
+    const byte = self.bytes[byte_idx];
+    const bit: u1 = @intCast((byte >> bit_idx) & 1);
+    return bit;
+}
+
+fn bitIdxToByteIdx(bit_idx: usize) struct { usize, u3 } {
+    return .{
+        bit_idx >> 3,
+        @intCast(bit_idx & 0b111),
+    };
+}
+/// Returns the minimum number of bytes necessary
+/// to represent a number of bits.
+fn numBitsToNumBytes(num_bits: usize) usize {
+    const partial_byte = num_bits & 0b111 != 0;
+    const bytes_floor = num_bits >> 3;
+    const bytes_ceil = bytes_floor + @intFromBool(partial_byte);
+    return bytes_ceil;
+}
+fn addOrOom(a: usize, b: usize) error{OutOfMemory}!usize {
+    const result, const overflow = @addWithOverflow(a, b);
+    if (overflow != 0) return error.OutOfMemory;
+    return result;
+}
+
+/// Deprecated in favor of `BitStack`.
 pub const Managed = struct {
     bytes: std.array_list.Managed(u8),
     bit_len: usize = 0,
