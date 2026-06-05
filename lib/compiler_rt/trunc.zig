4e844a9e78f1dc6a66bf8c840c345573492a74c3
Christophe Delage <christ@ophe.net>
2026-06-05 08:14:28 +0200

    Implement @trunc for all float types
    * Replaces the three nearly identical functions for f32, f64 and f128 with
      a single generic function that works for all float types.
    
    * Better tests.

--- lib/compiler_rt/trunc.zig
+++ lib/compiler_rt/trunc.zig
@@ -25,76 +25,23 @@ comptime {
 }
 
 pub fn __trunch(x: f16) callconv(.c) f16 {
-    // TODO: more efficient implementation
-    return @floatCast(truncf(x));
+    return impl(f16, x);
 }
 
 pub fn truncf(x: f32) callconv(.c) f32 {
-    const u: u32 = @bitCast(x);
-    var e = @as(i32, @intCast(((u >> 23) & 0xFF))) - 0x7F + 9;
-    var m: u32 = undefined;
-
-    if (e >= 23 + 9) {
-        return x;
-    }
-    if (e < 9) {
-        e = 1;
-    }
-
-    m = @as(u32, math.maxInt(u32)) >> @intCast(e);
-    if (u & m == 0) {
-        return x;
-    } else {
-        if (compiler_rt.want_float_exceptions) mem.doNotOptimizeAway(x + 0x1p120);
-        return @bitCast(u & ~m);
-    }
+    return impl(f32, x);
 }
 
 pub fn trunc(x: f64) callconv(.c) f64 {
-    const u: u64 = @bitCast(x);
-    var e = @as(i32, @intCast(((u >> 52) & 0x7FF))) - 0x3FF + 12;
-    var m: u64 = undefined;
-
-    if (e >= 52 + 12) {
-        return x;
-    }
-    if (e < 12) {
-        e = 1;
-    }
-
-    m = @as(u64, math.maxInt(u64)) >> @intCast(e);
-    if (u & m == 0) {
-        return x;
-    } else {
-        if (compiler_rt.want_float_exceptions) mem.doNotOptimizeAway(x + 0x1p120);
-        return @bitCast(u & ~m);
-    }
+    return impl(f64, x);
 }
 
 pub fn __truncx(x: f80) callconv(.c) f80 {
-    // TODO: more efficient implementation
-    return @floatCast(truncq(x));
+    return impl(f80, x);
 }
 
 pub fn truncq(x: f128) callconv(.c) f128 {
-    const u: u128 = @bitCast(x);
-    var e = @as(i32, @intCast(((u >> 112) & 0x7FFF))) - 0x3FFF + 16;
-    var m: u128 = undefined;
-
-    if (e >= 112 + 16) {
-        return x;
-    }
-    if (e < 16) {
-        e = 1;
-    }
-
-    m = @as(u128, math.maxInt(u128)) >> @intCast(e);
-    if (u & m == 0) {
-        return x;
-    } else {
-        if (compiler_rt.want_float_exceptions) mem.doNotOptimizeAway(x + 0x1p120);
-        return @bitCast(u & ~m);
-    }
+    return impl(f128, x);
 }
 
 pub fn truncl(x: c_longdouble) callconv(.c) c_longdouble {
@@ -106,44 +53,123 @@ pub fn truncl(x: c_longdouble) callconv(.c) c_longdouble {
     }
 }
 
+/// returns 'x' truncated to the nearest int towards zero
+inline fn impl(T: type, x: T) T {
+    const Uint = @Int(.unsigned, @bitSizeOf(T));
+    const u: Uint = @bitCast(x);
+
+    const mbits = math.floatMantissaBits(T);
+    const fbits = math.floatFractionalBits(T);
+    const ebits = math.floatExponentBits(T);
+    const explicit = mbits - fbits;
+
+    const emask = (1 << ebits) - 1;
+    const bias = emask >> 1;
+
+    const expn: i32 = @intCast((u >> mbits) & emask);
+    // if the integer bit is explicit (ie T == f80), we need to shift past it
+    const e0: i32 = ebits + 1 + explicit;
+    const e: i32 = expn - bias + e0;
+
+    // if expn - bias >= fbits, x is an integer.
+    if (e >= fbits + e0) return x;
+
+    // @intCast(shift) is safe because:
+    // * At this point, e < fbits + e0 and fbits + e0 == @bitSizeOf(T)
+    // * shift <= 0 would mean e <= 0 and e >= e0, but e0 > 0
+    const shift = if (e < e0) 1 else e;
+    const mask = ~@as(Uint, 0) >> @intCast(shift);
+
+    // At this point, the 1 bits of mask correspond to the fractional bits of x.
+    // x is an integer iff they are all zero.
+    if (u & mask == 0) return x;
+
+    if (compiler_rt.want_float_exceptions) {
+        // large enough to raise inexact but small enough to not raise overflow
+        const large: T = switch (T) {
+            f16 => 0x1p14,
+            f32 => 0x1p126,
+            f64 => 0x1p1022,
+            f80 => 0x1p16382,
+            f128 => 0x1p16382,
+            else => @compileError("not a floating point type"),
+        };
+        std.mem.doNotOptimizeAway(x + large);
+    }
+
+    // zero out the fractional bits of x to produce the resulting integer
+    return @bitCast(u & ~mask);
+}
+
+fn testImpl(T: type) !void {
+    const fbits = math.floatFractionalBits(T);
+    const U = @Int(.unsigned, @bitSizeOf(T));
+
+    var u: U = 0;
+    while (u < math.maxInt(U) / 3) {
+        defer u = u + u / 3 + 1;
+        const x: T = @floatFromInt(u);
+
+        for ([_]T{ 0.0, 0.3, 0.5, 0.7 }) |frac| {
+            const y = x + frac;
+
+            const expected = if (u < (1 << fbits)) x else y;
+
+            try expect(impl(T, y) == expected);
+            try expect(impl(T, -y) == -expected);
+
+            if (expected == 0.0) {
+                try expect(math.signbit(impl(T, y)) == math.signbit(expected));
+                try expect(math.signbit(impl(T, -y)) == math.signbit(-expected));
+            }
+        }
+    }
+}
+
+test "trunc16" {
+    try testImpl(f16);
+}
+
 test "trunc32" {
-    try expect(truncf(1.3) == 1.0);
-    try expect(truncf(-1.3) == -1.0);
-    try expect(truncf(0.2) == 0.0);
+    try testImpl(f32);
 }
 
 test "trunc64" {
-    try expect(trunc(1.3) == 1.0);
-    try expect(trunc(-1.3) == -1.0);
-    try expect(trunc(0.2) == 0.0);
+    try testImpl(f64);
+}
+
+test "trunc80" {
+    try testImpl(f80);
 }
 
 test "trunc128" {
-    try expect(truncq(1.3) == 1.0);
-    try expect(truncq(-1.3) == -1.0);
-    try expect(truncq(0.2) == 0.0);
+    try testImpl(f128);
+}
+
+fn testImplSpecial(T: type) !void {
+    try expect(math.isPositiveZero(impl(T, 0.0)));
+    try expect(math.isNegativeZero(impl(T, -0.0)));
+    try expect(math.isPositiveInf(impl(T, math.inf(T))));
+    try expect(math.isNegativeInf(impl(T, -math.inf(T))));
+    try expect(math.isNan(impl(T, math.nan(T))));
+}
+
+test "trunc16.special" {
+    try testImplSpecial(f16);
 }
 
 test "trunc32.special" {
-    try expect(truncf(0.0) == 0.0); // 0x3F800000
-    try expect(truncf(-0.0) == -0.0);
-    try expect(math.isPositiveInf(truncf(math.inf(f32))));
-    try expect(math.isNegativeInf(truncf(-math.inf(f32))));
-    try expect(math.isNan(truncf(math.nan(f32))));
+    try testImplSpecial(f32);
 }
 
 test "trunc64.special" {
-    try expect(trunc(0.0) == 0.0);
-    try expect(trunc(-0.0) == -0.0);
-    try expect(math.isPositiveInf(trunc(math.inf(f64))));
-    try expect(math.isNegativeInf(trunc(-math.inf(f64))));
-    try expect(math.isNan(trunc(math.nan(f64))));
+    try testImplSpecial(f64);
+}
+
+test "trunc80.special" {
+    try testImplSpecial(f80);
 }
 
 test "trunc128.special" {
-    try expect(truncq(0.0) == 0.0);
-    try expect(truncq(-0.0) == -0.0);
-    try expect(math.isPositiveInf(truncq(math.inf(f128))));
-    try expect(math.isNegativeInf(truncq(-math.inf(f128))));
-    try expect(math.isNan(truncq(math.nan(f128))));
+    try testImplSpecial(f128);
 }
