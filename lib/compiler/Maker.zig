63abea9c5a59e52f0859f91b1074398f18e2f592
carmooo <joao.carmo@paddle.com>
2026-06-15 12:27:09 +0100

    Maker: use time parsing func for debounce

--- lib/compiler/Maker.zig
+++ lib/compiler/Maker.zig
@@ -197,7 +197,7 @@ pub fn main(init: process.Init.Minimal) !void {
     var color: Color = .settingFromEnvironment(&graph.environ_map);
     var watch = false;
     var fuzz: ?Fuzz.Mode = null;
-    var debounce_interval_ms: u16 = 50;
+    var debounce_interval_ns: u16 = 50;
     var webui_listen: ?Io.net.IpAddress = null;
     var debug_pkg_config = false;
     var run_args: ?[]const []const u8 = null;
@@ -291,11 +291,11 @@ pub fn main(init: process.Init.Minimal) !void {
             } else if (mem.eql(u8, arg, "--debounce")) {
                 const next_arg = nextArg(args, &arg_idx) orelse
                     fatalWithHint("expected u16 after {q}", .{arg});
-                debounce_interval_ms = std.fmt.parseUnsigned(u16, next_arg, 0) catch |err| {
-                    fatal("unable to parse debounce interval {q} as unsigned 16-bit integer: {t}", .{
-                        next_arg, err,
-                    });
-                };
+                debounce_interval_ns = std.fmt.parseTimeSuffixNs(next_arg) catch |err|
+                    fatal(
+                        "invalid debounce {q}: {t}",
+                        .{ next_arg, err },
+                    );
             } else if (mem.eql(u8, arg, "--webui")) {
                 if (webui_listen == null) webui_listen = .{ .ip6 = .loopback(0) };
             } else if (mem.startsWith(u8, arg, "--webui=")) {
@@ -661,7 +661,7 @@ pub fn main(init: process.Init.Minimal) !void {
         }) catch &caption_buf;
         var debouncing_node = main_progress_node.start(caption, 0);
         var in_debounce = false;
-        while (true) switch (try w.wait(if (in_debounce) .{ .ms = debounce_interval_ms } else .none)) {
+        while (true) switch (try w.wait(if (in_debounce) .{ .ns = debounce_interval_ns } else .none)) {
             .timeout => {
                 assert(in_debounce);
                 debouncing_node.end();
